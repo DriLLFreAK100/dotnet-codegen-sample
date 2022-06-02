b11ddb404439f5540eedd0e23017a470ef43b56c
drillfreak100 <blusky_8893@hotmail.com>
2022-06-03 02:13:26 +0800

    feat: include console generator sample

---
+++ SampleConsoleGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using CodeGenerator;
+using CodeGenerator.Models;
+using SampleConsoleGenerator;
+using System.Reflection;
+using System.Text.Json;
+
+TypeScript generator = new (new Option()
+{
+    IsDryRun = false,
+    RelativeBaseOutputPath="../../../GeneratedOutputs",
+    TargetAssemblies = new()
+    {
+        Assembly.GetAssembly(typeof(InterfaceRoot)),
+    }
+});
+
+var outputs = generator.Generate();
+
+Console.WriteLine(JsonSerializer.Serialize(outputs, new JsonSerializerOptions()
+{
+    WriteIndented = true,
+}));
+
+Console.Read();
---
+++ SampleConsoleGenerator/SampleModels.cs
@@ -0,0 +1,44 @@
+﻿using CodeGenerator.Attributes;
+
+namespace SampleConsoleGenerator
+{
+    [GenerateTs]
+    public class InterfaceRoot
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedTime { get; set; }
+
+        public InterfaceNonAnnotatedChild NonAnnotatedChild { get; set; }
+
+        public InterfaceChild AnnotatedChild { get; set; }
+
+        public List<int> ChildIds { get; set; }
+
+        public List<InterfaceNonAnnotatedChild> UnknownChildren { get; set; }
+
+        public List<InterfaceChild> Children { get; set; }
+
+        public Dictionary<int, string> BuiltInDict { get; set; }
+
+        public Dictionary<int, InterfaceChild> CustomDict { get; set; }
+
+        public Dictionary<int, List<InterfaceChild>> CustomListInDict { get; set; }
+
+        public Dictionary<int, Dictionary<int, List<InterfaceChild>>> CrazyComplexType { get; set; }
+    }
+
+    public class InterfaceNonAnnotatedChild
+    {
+        public int Id { get; set; }
+    }
+
+    [GenerateTs]
+    public class InterfaceChild
+    {
+        public int Id { get; set; }
+    }
+}
+
