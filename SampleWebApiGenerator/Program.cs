14758de68eba34f78897d1a0a11075a72f62bf68
drillfreak100 <blusky_8893@hotmail.com>
2022-06-03 15:08:11 +0800

    feat: sample usage with web api

---
+++ SampleWebApiGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using CodeGenerator;
+using CodeGenerator.Models;
+using SampleWebApiGenerator;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Configure Code Generator
+    new TypeScript(new Option()
+    {
+        IsDryRun = false,
+        RelativeBaseOutputPath = "../../../GeneratedOutputs",
+        TargetAssemblies = new()
+        {
+            Assembly.GetAssembly(typeof(WeatherForecast)),
+        }
+    }).Generate();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
